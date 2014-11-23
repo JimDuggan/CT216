ca586eebb091a27aa656c6244585cd7e2616b8f9
Jim Duggan <jim.duggan@nuigalway.ie>
2014-11-23 19:19:34 +0000

    Django Example

---
+++ Django Examples/mysite/mysite/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render_to_response
+from time import strftime, gmtime
+
+def welcome(request):
+    return render_to_response('welcome.html')
+
+def test(request):
+    orderRec = {'name'         : 'John',
+                'order_number' : '1111111FFFFF',
+                'date'         : strftime("%Y-%m-%d %H:%M:%S", gmtime()),
+                'items'        : [{'ID': 'MX101','Description':'Laptop','Qty':1},
+                                 {'ID': 'MX102','Description':'Mouse','Qty':2}]
+               }
+    return render_to_response('test.html', orderRec)
+
+
+
+
+
